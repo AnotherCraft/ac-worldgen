7d4ecfe0673193415be6563bb75a8ed589fa6066
Danol <czdanol@gmail.com>
2022-04-19 15:53:51 +0200

    Some fixes

--- src/main.cpp
+++ src/main.cpp
@@ -38,241 +38,255 @@ int main(int argc, char *argv[]) {
 	setmode(fileno(stdout), O_BINARY);
 #endif
 
-	std::vector<std::string> files, lookupDirs;
-	std::unordered_map<std::string, BlockID> blockMapping;
-	size_t seed = 0;
-	size_t threadCount = std::min<size_t>(std::thread::hardware_concurrency() - 2, 4);
-	bool exportList = false;
-	bool showHelp = argc < 2;
+	std::vector<std::thread> pool;
 
-	size_t argi = 1;
-	const auto popArg = [&](const std::string &def = {}) {
-		return (argi >= argc) ? def : std::string(argv[argi++]);
-	};
+	try {
+		std::vector<std::string> files, lookupDirs;
+		std::unordered_map<std::string, BlockID> blockMapping;
+		size_t seed = 0;
+		size_t threadCount = std::min<size_t>(std::thread::hardware_concurrency() - 2, 4);
+		bool exportList = false;
+		bool showHelp = argc < 2;
 
-	while(argi < argc) {
-		const std::string arg = popArg();
-		if(arg == "-f" || arg == "--sourceFile")
-			files.push_back(popArg());
+		size_t argi = 1;
+		const auto popArg = [&](const std::string &def = {}) {
+			return (argi >= argc) ? def : std::string(argv[argi++]);
+		};
 
-		else if(arg == "-d" || arg == "--lookupDirectory")
-			lookupDirs.push_back(popArg());
+		while(argi < argc) {
+			const std::string arg = popArg();
+			if(arg == "-f" || arg == "--sourceFile")
+				files.push_back(popArg());
 
-		else if(arg == "-s" || arg == "--seed")
-			seed = std::stoi(popArg());
+			else if(arg == "-d" || arg == "--lookupDirectory")
+				lookupDirs.push_back(popArg());
 
-		else if(arg == "-m" || arg == "--blockMapping") {
-			const std::string str = popArg();
-			size_t offset = 0;
-			while(true) {
-				const size_t eqsep = str.find('=', offset);
-				if(eqsep == std::string::npos)
-					break;
+			else if(arg == "-s" || arg == "--seed")
+				seed = std::stoi(popArg());
 
-				const size_t endsep = str.find(',', eqsep);
+			else if(arg == "-m" || arg == "--blockMapping") {
+				const std::string str = popArg();
+				size_t offset = 0;
+				while(true) {
+					const size_t eqsep = str.find('=', offset);
+					if(eqsep == std::string::npos)
+						break;
 
-				const std::string uid = str.substr(offset, eqsep - offset - 1);
-				const BlockID id = (BlockID) std::stoi(str.substr(eqsep + 1, endsep));
-				blockMapping.insert_or_assign(uid, id);
+					const size_t endsep = str.find(',', eqsep);
 
-				if(endsep == std::string::npos)
-					break;
+					const std::string uid = str.substr(offset, eqsep - offset);
+					const BlockID id = (BlockID) std::stoi(str.substr(eqsep + 1, endsep));
 
-				offset = endsep + 1;
+					blockMapping.insert_or_assign(uid, id);
+
+					if(endsep == std::string::npos)
+						break;
+
+					offset = endsep + 1;
+				}
+			}
+
+			else if(arg == "-t" || arg == "--threadCount")
+				threadCount = stoi(popArg());
+
+			else if(arg == "--functionList") {
+				std::cout << "# WOGLAC function list\n";
+				std::cout << "Auto generated from the source code.\n\n";
+
+				WorldGenAPI::functions().generateDocumentation();
+				return 0;
+			}
+
+			else if(arg == "--exportList")
+				exportList = true;
+
+			else if(arg == "--help" || arg == "-h")
+				showHelp = true;
+
+			else {
+				std::cout << std::format("Unknown parameter '{}'.\n", arg);
+				return 1;
 			}
 		}
 
-		else if(arg == "-t" || arg == "--threadCount")
-			threadCount = stoi(popArg());
+		if(showHelp) {
+			std::cout << "AnotherCraft worldgen subsystem. After starting, the communication with the app is realized throu stdin and stdout pipes, using single-line JSON-encoded messages (with some asterisk).\nMore info on https://github.com/AnotherCraft/ac-worldgen.\n";
+			std::cout << "\n\nUsage: ac-worldgen (args)\n\n";
 
-		else if(arg == "--functionList") {
-			std::cout << "# WOGLAC function list\n";
-			std::cout << "Auto generated from the source code.\n\n";
-
-			WorldGenAPI::functions().generateDocumentation();
+			std::cout << "-f <f> | --sourceFile <f>\nSource file (accepts multiple).\n\n";
+			std::cout << "-d <d> | --lookupDirectory <d>\nLookup directory for resources (for example .vox files for structure generator, accepts multiple).\n\n";
+			std::cout << "-s <s> | --seed <s>\nSeed for the worldgen (number).\n\n";
+			std::cout << "-m <m> | --blockMapping <m>\nBlock UID (string) -> ID (uint16_t) mapping in format 'uid=id,uid2=id2,uid3=id3'.\nBlock UIDs have to be prefixed with 'block.', for example 'block.core.air'.\nID 0 is reserved for 'block.air'.\nID 1 is reserved for 'block.undefined'.\n\n";
+			std::cout << "--functionList\nEmits a function list in the Markdown format.";
+			std::cout << "--exportList\nCompiles the source files and prints out the list of exports.";
 			return 0;
 		}
 
-		else if(arg == "--exportList")
-			exportList = true;
+		// Setup WorldGenAPI
+		WorldGenAPI_CPU wgapi;
+		{
+			wgapi.setSeed(WorldGenSeed(seed));
 
-		else if(arg == "--help" || arg == "-h")
-			showHelp = true;
-
-		else {
-			std::cout << std::format("Unknown parameter '{}'.\n", arg);
-			return 1;
+			// Block mapping
+			blockMapping["block.air"] = blockID_air;
+			blockMapping["block.undefined"] = blockID_undefined;
+			wgapi.setBlockUIDMapping(blockMapping);
 		}
-	}
 
-	if(showHelp) {
-		std::cout << "AnotherCraft worldgen subsystem. After starting, the communication with the app is realized throu stdin and stdout pipes, using single-line JSON-encoded messages (with some asterisk).\nMore info on https://github.com/AnotherCraft/ac-worldgen.\n";
-		std::cout << "\n\nUsage: ac-worldgen (args)\n\n";
+		// Compile source files
+		std::unordered_map<std::string, WGA_Value *> exports;
+		{
+			WGLCompiler wgc;
 
-		std::cout << "-f <f> | --sourceFile <f>\nSource file (accepts multiple).\n\n";
-		std::cout << "-d <d> | --lookupDirectory <d>\nLookup directory for resources (for example .vox files for structure generator, accepts multiple).\n\n";
-		std::cout << "-s <s> | --seed <s>\nSeed for the worldgen (number).\n\n";
-		std::cout << "-m <m> | --blockMapping <m>\nBlock UID (string) -> ID (uint16_t) mapping in format 'uid=id,uid2=id2,uid3=id3'.\nBlock UIDs have to be prefixed with 'block.', for example 'block.core.air'.\nID 0 is reserved for 'block.air'.\nID 1 is reserved for 'block.undefined'.\n\n";
-		std::cout << "--functionList\nEmits a function list in the Markdown format.";
-		std::cout << "--exportList\nCompiles the source files and prints out the list of exports.";
-		return 0;
-	}
+			wgc.setLookupDirectories(lookupDirs);
 
-	// Setup WorldGenAPI
-	WorldGenAPI_CPU wgapi;
-	{
-		wgapi.setSeed(WorldGenSeed(seed));
+			for(const std::string &filename: files)
+				wgc.addFile(std::make_shared<WGLFile>(filename));
 
-		// Block mapping
-		blockMapping["block.air"] = blockID_air;
-		blockMapping["block.undefined"] = blockID_undefined;
-		wgapi.setBlockUIDMapping(blockMapping);
-	}
+			wgc.compile();
+			exports = wgc.construct(wgapi);
+		}
 
-	// Compile source files
-	std::unordered_map<std::string, WGA_Value *> exports;
-	{
-		WGLCompiler wgc;
+		if(exportList) {
+			for(auto it = exports.begin(), e = exports.end(); it != e; it++)
+				std::cout << std::format("%1: %2\n", it->first, WGA_Value::typeNames.at(it->second->valueType()));
 
-		wgc.setLookupDirectories(lookupDirs);
+			return 0;
+		}
 
-		for(const std::string &filename: files)
-			wgc.addFile(std::make_shared<WGLFile>(filename));
+		for(size_t i = 0; i < threadCount; i++) {
+			pool.push_back(std::thread([] {
+				std::function < void() > job;
 
-		wgc.compile();
-		exports = wgc.construct(wgapi);
-	}
+				{
+					std::unique_lock lock(jobsMutex);
+					while(jobs.empty())
+						newJobCondition.wait(lock);
 
-	if(exportList) {
-		for(auto it = exports.begin(), e = exports.end(); it != e; it++)
-			std::cout << std::format("%1: %2\n", it->first, WGA_Value::typeNames.at(it->second->valueType()));
+					job = jobs.front();
+					jobs.pop();
+				}
 
-		return 0;
-	}
+				job();
 
-	std::vector<std::thread> pool;
-	for(size_t i = 0; i < threadCount; i++) {
-		pool.push_back(std::thread([] {
-			std::function<void()> job;
+				{
+					std::unique_lock lock(jobsMutex);
+					runningJobs--;
+					jobEndCondition.notify_all();
+				}
+			}));
+		}
 
-			{
-				std::unique_lock lock(jobsMutex);
-				while(jobs.empty())
-					newJobCondition.wait(lock);
+		// Main stdin loop
+		while(true) {
+			std::string type;
+			std::cin >> type;
 
-				job = jobs.front();
-				jobs.pop();
-			}
+			if(type.empty() && std::cin.eof())
+				break;
 
-			job();
+			if(type == "getData") {
+				BlockWorldPos pos;
+				std::cin >> pos.x() >> pos.y() >> pos.z();
+				pos = pos & ~blockInChunkPosMask;
 
-			{
-				std::unique_lock lock(jobsMutex);
-				runningJobs--;
-				jobEndCondition.notify_all();
-			}
-		}));
-	}
+				std::string var;
+				std::cin >> var;
+				const auto valp = exports.find(var);
+				if(valp == exports.end()) {
+					std::unique_lock _l(stdoutMutex);
+					std::cout << "message Export does not exist: " << var << "\n";
+					continue;
+				}
+				WGA_Value *val = valp->second;
 
-	// Main stdin loop
-	while(true) {
-		if(std::cin.eof())
-			break;
+				if(val->symbolType() != WGA_Value::SymbolType::Value) {
+					std::unique_lock _l(stdoutMutex);
+					std::cout << "Export symbol is not a variable\n";
+					continue;
+				}
 
-		std::string type;
-		std::cin >> type;
+				std::string valueType;
+				std::cin >> valueType;
+				if(WGA_Value::typeNames.at(val->valueType()) != valueType) {
+					std::unique_lock _ul(stdoutMutex);
+					std::cout << std::format("Export '{}' is of type '{}', but '{}' expected.\n", var, WGA_Value::typeNames.at(val->valueType()), valueType);
+					return 1;
+				}
 
-		if(type == "getData") {
-			BlockWorldPos pos;
-			std::cin >> pos.x() >> pos.y() >> pos.z();
-			pos = pos & ~blockInChunkPosMask;
-
-			std::string var;
-			std::cin >> var;
-			const auto valp = exports.find(var);
-			if(valp == exports.end()) {
-				std::unique_lock _l(stdoutMutex);
-				std::cout << "message Export does not exist: " << var << "\n";
-				continue;
-			}
-			WGA_Value *val = valp->second;
-
-			if(val->symbolType() != WGA_Value::SymbolType::Value) {
-				std::unique_lock _l(stdoutMutex);
-				std::cout << "Export symbol is not a variable\n";
-				continue;
-			}
-
-			std::string valueType;
-			std::cin >> valueType;
-			if(WGA_Value::typeNames.at(val->valueType()) != valueType) {
-				std::unique_lock _ul(stdoutMutex);
-				std::cout << std::format("Export '{}' is of type '{}', but '{}' expected.\n", var, WGA_Value::typeNames.at(val->valueType()), valueType);
-				return 1;
-			}
-
-			struct Data {
-				std::vector<char> data;
-				size_t recordCount;
-			};
-			std::function<Data()> f;
-
-			const auto genf = [val, pos]<WGA_Value::ValueType VT>() {
-				return [val, pos] {
-					ZoneScopedN("getData");
-
-					auto h = WGA_ValueWrapper_CPU<VT>(static_cast<WGA_Value_CPU *>(val)).dataHandle(pos);
-					Data r{
-						.recordCount = h.size
-					};
-
-					const size_t bytes = sizeof(WGA_ValueRec_CPU<VT>::T) * h.size;
-					r.data.resize(bytes);
-					memcpy(r.data.data(), reinterpret_cast<const char *>(h.data), bytes);
-					return r;
+				struct Data {
+					std::vector<char> data;
+					size_t recordCount;
 				};
-			};
+				std::function < Data() > f;
 
-			if(val->valueType() == WGA_Value::ValueType::Float)
-				f = genf.operator ()<WGA_Value::ValueType::Float>();
+				const auto genf = [val, pos]<WGA_Value::ValueType VT>() {
+					return [val, pos] {
+						ZoneScopedN("getData");
 
-			else if(val->valueType() == WGA_Value::ValueType::Block)
-				f = genf.operator ()<WGA_Value::ValueType::Block>();
+						auto h = WGA_ValueWrapper_CPU<VT>(static_cast<WGA_Value_CPU *>(val)).dataHandle(pos);
+						Data r{
+							.recordCount = h.size
+						};
 
-			else {
-				std::unique_lock _l(stdoutMutex);
-				std::cout << std::format("Unsupported export value type: {}", WGA_Value::typeNames.at(val->valueType()));
-				return 1;
+						const size_t bytes = sizeof(WGA_ValueRec_CPU<VT>::T) * h.size;
+						r.data.resize(bytes);
+						memcpy(r.data.data(), reinterpret_cast<const char *>(h.data), bytes);
+						return r;
+					};
+				};
+
+				if(val->valueType() == WGA_Value::ValueType::Float)
+					f = genf.operator ()<WGA_Value::ValueType::Float>();
+
+				else if(val->valueType() == WGA_Value::ValueType::Block)
+					f = genf.operator ()<WGA_Value::ValueType::Block>();
+
+				else {
+					std::unique_lock _l(stdoutMutex);
+					std::cout << std::format("Unsupported export value type: {}", WGA_Value::typeNames.at(val->valueType()));
+					return 1;
+				}
+
+				const auto job = [f, pos, var] {
+					const Data d = f();
+					std::unique_lock _ul(stdoutMutex);
+
+					std::cout << "data " << var << ' ' << pos.x() << ' ' << pos.y() << ' ' << pos.z() << ' ' << d.data.size() << "\n";
+					std::cout.write(d.data.data(), d.data.size());
+				};
+
+				{
+					std::unique_lock _ml(jobsMutex);
+					runningJobs++;
+					jobs.push(job);
+					newJobCondition.notify_one();
+				}
 			}
 
-			const auto job = [f, pos, var] {
-				const Data d = f();
-				std::unique_lock _ul(stdoutMutex);
+			else
+				throw std::exception(std::format("Unknown message type: {}", type).c_str());
 
-				std::cout << "data" << var << pos.x() << pos.y() << pos.z() << d.data.size() << "\n";
-				std::cout.write(d.data.data(), d.data.size());
-			};
-
-			{
-				std::unique_lock _ml(jobsMutex);
-				runningJobs++;
-				jobs.push(job);
-				newJobCondition.notify_one();
-			}
 		}
 
-		else {
-			std::unique_lock _l(stdoutMutex);
-			std::cout << "Unknown message type:" << type << "\n";
-			return 1;
+
+		{
+			std::unique_lock l(jobsMutex);
+			while(runningJobs)
+				jobEndCondition.wait(l);
 		}
+
+		{
+			for(std::thread &t: pool)
+				t.detach();
+		}
+
 	}
-
-	// Wait for all jobs to end
-	{
-		std::unique_lock l(jobsMutex);
-		while(runningJobs)
-			jobEndCondition.wait(l);
+	catch(const std::exception &e) {
+		std::cout << "message " << e.what();
+		for(std::thread &t: pool)
+			t.detach();
+		return 1;
 	}
 
 	return 0;
