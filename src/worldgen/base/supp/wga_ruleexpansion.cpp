b88688c95c04632c67ae08744324d759e1628776
Danol <czdanol@gmail.com>
2022-04-22 10:13:43 +0200

    Fix rule expansion priority

--- src/worldgen/base/supp/wga_ruleexpansion.cpp
+++ src/worldgen/base/supp/wga_ruleexpansion.cpp
@@ -11,25 +11,29 @@ WGA_RuleExpansion::WGA_RuleExpansion(WGA_Rule *rule) {
 
 	rule_ = rule;
 	static const PragmaList pg{
-		{"priority",         1.0f},
+		{"priority",         999.0f},
 		{"probabilityRatio", 100.0f}
 	};
 	pragmas_ = pg;
-
-	// rule -> false; Has the lowest priority by default
-	if(!component_ && !targetRule_)
-		setPragma("priority", 9999.0f);
 }
 
 WGA_RuleExpansion::WGA_RuleExpansion(WGA_Rule *rule, WGA_Component *component, const std::string &node) : WGA_RuleExpansion(rule) {
+	ASSERT(component);
+
 	component_ = component;
 	node_ = node;
 	targetType_ = TargetType::expandsToComponent;
+
+	setPragma("priority", 1.0f);
 }
 
 WGA_RuleExpansion::WGA_RuleExpansion(WGA_Rule *rule, WGA_Rule *targetRule) : WGA_RuleExpansion(rule) {
+	ASSERT(targetRule);
+
 	targetRule_ = targetRule;
 	targetType_ = TargetType::expandsToRule;
+
+	setPragma("priority", 1.0f);
 }
 
 WGA_Symbol::SymbolType WGA_RuleExpansion::symbolType() const {
