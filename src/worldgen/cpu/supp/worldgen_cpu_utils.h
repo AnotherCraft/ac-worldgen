ad23c5b47adf5abca0ad043040490d531636f566
Danol <czdanol@gmail.com>
2022-04-28 11:51:31 +0200

    Fix rand2D not being random enough

--- src/worldgen/cpu/supp/worldgen_cpu_utils.h
+++ src/worldgen/cpu/supp/worldgen_cpu_utils.h
@@ -12,6 +12,13 @@ public:
 	static uint32_t hash(const Vector2U32 &v, uint32_t seed);
 	static uint32_t hash(const Vector3U32 &v, uint32_t seed);
 
+	template<typename... Args>
+	static uint32_t hashMulti(uint32_t seed, const Args& ...args) {
+		uint32_t r = seed;
+		((r = hash(args, r)), ...);
+		return r;
+	}
+
 
 };
 
